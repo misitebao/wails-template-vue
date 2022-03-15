95bf30e5ff4c2e2ba67fb59d650e47add5b8fcdf
misitebao <i@misitebao.com>
2022-03-15 23:39:17 +0800

    refactor: update frontend project directory and upgrade dependencies

--- frontend/packages/javascript/vite.config.js
+++ frontend/packages/javascript/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     },
   },
   build: {
-    outDir:"../dist",
+    outDir:"../../dist",
     rollupOptions: {
       output: {
         entryFileNames: `assets/[name].js`,
